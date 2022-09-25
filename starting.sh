5b00f892a1070864627e4abd03c4d7a134fbf8de
benntuecon <zxc123598tw@gmail.com>
2022-09-24 19:18:42 -0700

    ready for sudo docker-compose up

---
+++ starting.sh
@@ -0,0 +1,11 @@
+sudo yum update -y
+sudo yum install git -y
+
+sudo yum install docker -y
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.11.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+sudo dockerd
+
+
